e0155bb598e1d0ffde978e66c84c295359f3d7fc
khushmeetSingh <skhushmeet7@gmail.com>
2023-02-24 01:47:13 -0500

    Text fills but without space

--- pages/landing-page/index.tsx
+++ pages/landing-page/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Banner, Bitmoji, NameHeading } from './styles'
+import { Banner, BannerText, Bitmoji, NameHeading, TextDiv } from './styles'
 import BITMOJI from '/images/Homepage-bitmoji.png';
 import GlobalStyle from './global-style';
 import { Head } from 'next/document';
 import Script from 'next/script';
 
 //{bitmojiPath,firstHeadingText,secondHeadingText}:LandingPageProps)=>
-const LandingPage=function(){
-    return<>
-    <Head>
-        {/* <Script src='./index.js' strategy='lazyOnload'></Script> */}
-    </Head>
-   <GlobalStyle/>
-    <Banner>
-        <Bitmoji src={BITMOJI.src} height={"700px"} width={"auto"}/>
-        <NameHeading>Khushmeet Singh</NameHeading>
-    </Banner>
+const LandingPage = function () {
+    return <>
+        <GlobalStyle />
+        <Banner>
+            <Bitmoji src={BITMOJI.src} height={"700px"} width={"auto"} />
+            <TextDiv>
+                <BannerText>Hello, I am</BannerText>
+                <NameHeading>KhushmeetSingh</NameHeading>
+            </TextDiv>
+        </Banner>
     </>
 }
 
--- pages/landing-page/styles.tsx
+++ pages/landing-page/styles.tsx
@@ -1,9 +1,16 @@
+//This styles file follows top to bottom approach
+//The parent element being the root of the tree and the childen being its leaves
+
 import styled, { keyframes } from 'styled-components'
 import { Keyframes } from 'styled-components'
 
-
-const h1Size='130px';
-const h2Size
+const h1Size = '130px';
+const h2Size = '75px';
+const h3Size = '50px';
+const h4Size = '35px';
+const h5Size = '24px';
+const h6Size = '16px';
+const ParagraphSize = '12px';
 
 //Animation for background for the image as well the text's color
 const bgGradient = keyframes`
@@ -35,25 +42,66 @@ export const Banner = styled.div`
 `
 
 //Component for the img on banner
-export const Bitmoji=styled.img`
+export const Bitmoji = styled.img`
   margin-left:30px;
   display:inline
 `
 
-export const Text=styled.h2`
+export const Text = styled.h2`
 color:#fff;
 font-size:
 `
 
+export const TextDiv = styled.div`
+display:flex;
+flex-direction:column; 
+`
+const TextFillAnimation=keyframes`
+from{
+  width:0;
+}
+to{
+  width:100%;
+  color:#fff;
+}
+`
+
 //Component for the heading containing the name
-export const NameHeading=styled.h1`
-  font-size:130px;
-  display:inline;
+export const NameHeading = styled.h1`
+  &{
+    color:#100f10;
+  font-size:${h1Size};
+  // display:inline;
 
   -webkit-text-stroke:3.5px #fff;
   // background: -webkit-linear-gradient(315deg,#C89D7C,#5f4531);
   // -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-size:130%;
-  animation: ${bgGradient} 7s ease-in infinite alternate-reverse;
+  // -webkit-text-fill-color: transparent;
+  // background-size:130%;
+  // animation: ${bgGradient} 7s ease-in infinite alternate-reverse;
+  position:relative;
+  }
+  &:before {
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    color: #fff;
+    overflow: hidden;
+    position: absolute;
+    content: "KhushmeetSingh";
+    border-right : 2px solid #37b9f1;
+    -webkit-text-stroke : 0vw #2ecc71;
+    animation: ${TextFillAnimation} 3s ;
+    animation-iteration-count:1;
+  }
+`
+
+
+//Styled component for the text that will be written on the banner except for the name
+export const BannerText = styled.h2`
+font-size:${h3Size};
+color:#fff;
+display:inline;
+
 `
